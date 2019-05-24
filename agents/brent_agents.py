a914e525f58ff38522c349fead67aedd0a3820ee
Brent Lunghino <brentlunghino@gmail.com>
2019-05-24 07:59:12 -0400

    A little progress on AgentQ

--- agents/brent_agents.py
+++ agents/brent_agents.py
@@ -7,14 +7,71 @@ import pypownet.environment
 import pypownet.agent
 
 
+class AgentQ(Agent):
+    """
+    agent implementing Q learning
+    this agent is limited to 1 action per timestep
+    following https://github.com/simoninithomas/Deep_reinforcement_learning_Course/tree/master/Q%20learning
+    """
+    def __init__(self, environment, mode='test', qtable=None):
+        super().__init__(environment)
+        self.ioman = ActIOnManager(destination_path=os.path.join('saved_actions', 'AgentZero.csv'))
+        n_state = environment.observation_space.n
+        n_action = environment.action_space.n
+        # Q(S,A)
+        # note the Q table is initialized with the Agent. to train over multiple episodes the
+        # environment must be reset but the agent must persist
+        if isinstance(qtable, np.array) && qtable.shape == (n_state, n_action):
+            self.qtable = qtable
+        else:
+            self.qtable = np.zeros((n_state, n_action))
+        # todo these should probably live somewhere more accessible?
+        # hyperparameters
+        self.total_episodes = 15000        # Total episodes
+        self.learning_rate = 0.8           # Learning rate
+        self.max_steps = 99                # Max steps per episode
+        self.gamma = 0.95                  # Discounting rate
+        # Exploration parameters
+        self.epsilon = 1.0                 # Exploration rate
+        self.max_epsilon = 1.0             # Exploration probability at start
+        self.min_epsilon = 0.01            # Minimum exploration probability 
+        self.decay_rate = 0.005   
+
+    def act(self, observation):
+        # This agent needs to manipulate actions using grid contextual information, so the observation object needs
+        # to be of class pypownet.environment.Observation: convert from array or raise error if that is not the case
+        if not isinstance(observation, pypownet.environment.Observation):
+            try:
+                observation = self.environment.observation_space.array_to_observation(observation)
+            except Exception as e:
+                raise e
+        # Sanity check: an observation is a structured object defined in the environment file.
+        assert isinstance(observation, pypownet.environment.Observation)
+        action_space = self.environment.action_space
+        ## First we randomize a number
+        exp_exp_tradeoff = random.uniform(0, 1)
+        
+        ## If this number > greater than epsilon --> exploitation (taking the biggest Q value for this state)
+        if exp_exp_tradeoff > self.epsilon:
+            action_id = np.argmax(self.qtable[observation,:])
+            action = action_space.get_do_nothing_action()
+            action[action_id] = 1
+        # Else doing a random choice --> exploration
+        else:
+            action = action_space.sample()
+        return action
+
+    def feed_reward(self, action, new_observation, reward_aslist):
+        """
+        update the q table using the new observation
+        """
+
 class AgentZero(Agent):
     """
     do a combination of both random actions, should do nothing about 1/3 of the time
     """
-
     def __init__(self, environment):
         super().__init__(environment)
-
         self.ioman = ActIOnManager(destination_path=os.path.join('saved_actions', 'AgentZero.csv'))
 
     def act(self, observation):
